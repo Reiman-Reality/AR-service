c50e6570af77cfafd8da03ec286fb376b576dc6a
Matt <mattvoz30@gmail.com>
2022-09-13 13:15:21 -0500

    fix tsc compiler

---
+++ server/dist/index.js
@@ -0,0 +1,15 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import serve from 'koa-static';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const server = new Koa();
+const router = new Router();
+const port = process.env.PORT ?? 8080;
+console.log(port);
+router.get('/home', async (ctx) => {
+});
+server.use(router.routes());
+server.use(serve('/static/models'));
+server.use(serve('/static/markers'));
+server.listen(port);
