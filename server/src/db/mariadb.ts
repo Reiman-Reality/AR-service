fafde37237466341185eb9956b560c7c4be1ae41
hoi phung <hhphung@iastate.edu>
2022-09-25 20:00:44 -0500

    update database work

--- server/src/db/mariadb.ts
+++ server/src/db/mariadb.ts
@@ -4,8 +4,40 @@ const connection = mariadb.createPool({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD
+    
 });
 
 export async function getAllEvents() {
     await connection.query('SELECT 1 + 1 AS solution');
 }
+
+//insert model data by passing an object
+export async function insert_model(model: d) {
+    await connection.query('INSERT INTO MODELS (model_id, file_path) VALUES (d.id, d.path) ');
+}
+
+
+// selete model data by id
+export async function select_model(int: id) {
+    await connection.query('SELECT * FROM MODELS WHERE id = ${id}');
+}
+
+
+//delete model data
+export async function delete_model(int: i) {
+    await connection.query('DELETE FROM MODELS WHERE id = ${id}');
+}
+
+// update the object
+export async function update_model(int: id) {
+    await connection.query('UPDATE MODELS SET path = ${newpath} WHERE id = ${id}');
+}
+
+/*
+`UPDATE MARKERTABLE 
+SET
+name = ${newname}
+path = ${newpath}
+WHERE
+ID = ${markerID}
+*/
