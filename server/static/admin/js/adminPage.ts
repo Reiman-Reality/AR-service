fab78dec5cda634f990c9e5c6cd857de7a82ed37
Evan Hilton <eohilton@iastate.edu>
2022-10-11 17:56:10 +0000

    Update adminPage.ts

--- server/static/admin/js/adminPage.ts
+++ server/static/admin/js/adminPage.ts
@@ -1,24 +1,16 @@
 const selectBox: HTMLElement = document.getElementById("left-column");
 const objectsFrame: HTMLElement = document.getElementById("nft-objects");
 const modelsFrame: HTMLElement = document.getElementById("ar-models");
-const eventsFrame: HTMLElement = document.getElementById("events");
 
 function changeFrame(): void {
     switch (this.value) {
         case "0":
             objectsFrame.removeAttribute("hidden");
             modelsFrame.setAttribute("hidden", "hidden");
-            eventsFrame.setAttribute("hidden", "hidden");
             break;
         case "1":
             objectsFrame.setAttribute("hidden", "hidden");
             modelsFrame.removeAttribute("hidden");
-            eventsFrame.setAttribute("hidden", "hidden");
-            break;
-        case "2":
-            objectsFrame.setAttribute("hidden", "hidden");
-            modelsFrame.setAttribute("hidden", "hidden");
-            eventsFrame.removeAttribute("hidden");
             break;
     }
 }
