29a2e905282b98dd990bd1cfe1245823b0cab954
Ian Gluesing <ian.gluesing@gmail.com>
2022-10-12 14:18:50 -0500

    Final commit, change screen growing to minimize

--- FrontEnd/growFrame.js
+++ FrontEnd/growFrame.js
@@ -1,10 +1,25 @@
 console.log("hihihihi");
 
-window.addEventListener("DOMContentLoaded", (event)=>{
-	document.querySelector("#loadingButton")?.addEventListener('click', (event)=>{
+let fullscreen = false
+
+function MaximizeMinimizeFunction() {
+	if(!fullscreen){
 		document.querySelector("#arIframe")?.classList.add("fullscreen");
 		document.querySelector(".header").classList.add('hide');
 		document.querySelectorAll(".intro").forEach( element =>{element.classList.add('hide')});
 		document.querySelector("#footer").classList.add('hide');
-	});
-});
+		let button = document.querySelector("#loadingButton")
+		button.value = "Minimize"
+
+		fullscreen = true
+	} else {
+		document.querySelector("#arIframe")?.classList.remove("fullscreen");
+		document.querySelector(".header").classList.remove('hide');
+		document.querySelectorAll(".intro").forEach( element =>{element.classList.remove('hide')});
+		document.querySelector("#footer").classList.remove('hide');
+		let button = document.querySelector("#loadingButton")
+		button.value = "Maximize"
+
+		fullscreen = false
+	}
+}
