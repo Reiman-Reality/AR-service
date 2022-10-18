7451da1c86b86115a6b469c2c72b823f647031b7
Evan Hilton <eohilton@iastate.edu>
2022-10-18 17:50:23 +0000

    Update arModels.ts

--- server/static/admin/js/arModels.ts
+++ server/static/admin/js/arModels.ts
@@ -19,11 +19,11 @@ window.addEventListener("DOMContentLoaded", async ()=>{
     }
 
     for (let i = 0; i < data.length; i++) {
-        makeTableEntry(data[i]);
+        makeModelTableEntry(data[i]);
     }
 });
 
-function makeTableEntry(data) : void {
+function makeModelTableEntry(data) : void {
     const tableEntryList : HTMLElement = document.getElementById("boxList");
     const tableEntry : HTMLElement = document.createElement("li");
     const tableEntryHeader : HTMLElement = document.createElement("div");
