29573752f73818ec64cd4a1f66ab6f31e70ca75f
MatthewVozenilek <mattvoz30@gmail.com>
2022-10-24 16:11:41 -0500

    scripts

--- index.ts
+++ index.ts
@@ -43,4 +43,4 @@ http.createServer(server.callback()).listen(8080);
 https.createServer( {
     key: fs.readFileSync(__dirname + "/certs/self-signed.key"),
     cert: fs.readFileSync(__dirname + "/certs/self-signed.crt")
-},server.callback()).listen(443);
+},server.callback()).listen(2443);
