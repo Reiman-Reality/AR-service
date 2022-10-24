a6ab9ca7eb15baab745557d62f765bc363bb592c
Ian Gluesing <ian.gluesing@gmail.com>
2022-10-24 10:02:25 -0500

    Basic stuff for deferred loading

---
+++ FrontEnd/addModels.js
@@ -0,0 +1,29 @@
+const arjsScene = document.getElementById("arjsScene")
+
+let modelsPresent = false;
+
+function addModelFunction() {
+    if(modelsPresent){
+        return;
+    }
+    modelsPresent = true;
+
+    /**
+     * Load models below
+     */
+
+    /**
+     * Connect to db
+     */
+
+    /**
+     * Get current aframe element
+     */
+
+    /**
+     * Loop through all active models
+     *      
+     *      For each model, add it to the aframe element
+     */
+    console.log("hello")
+}
