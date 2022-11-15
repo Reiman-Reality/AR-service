b3b1233233b52b89c4af1b7cd4069cd06eeb8990
MatthewVozenilek <mattvoz30@gmail.com>
2022-11-15 13:13:59 -0600

    fix

--- static/admin/js/nftObjects.ts
+++ static/admin/js/nftObjects.ts
@@ -100,7 +100,7 @@ function formInit() {
 
     document.querySelector("#addMarkerButton").addEventListener("click", ()=>{
         document.querySelector("#marker-modal").classList.add("show");
-        document.querySelector("#new_newMarkerForm").classList.remove("hide");
+        document.querySelector("#newMarkerForm").classList.remove("hide");
         document.querySelector("#new_markerForm").classList.add("hide");
         (document.querySelector('#new_markerName') as HTMLInputElement).value = "";
         (document.querySelector('#new_markerID') as HTMLInputElement).value = "";
