e1aef66cc5df31c1e63e41b5e2816a7bfb892e03
Ian Gluesing <ian.gluesing@gmail.com>
2022-11-16 11:28:22 -0600

    Attempting to make stuff work

--- FrontEnd/addModels.js
+++ FrontEnd/addModels.js
@@ -4,7 +4,7 @@ function beforeEntity(markerUrl) {
     return `<a-nft type=\"nft\" url=\"./${markerUrl}\" smooth=\"true\" smoothCount=\"10\" smoothTolerance=\".01\" smoothThreshold=\"5\">`
 }
 
-function objEntity(objName){
+function objEntity(objName) {
     return `<a-entity obj-model="url(./models/${objName}.obj); mtl: url(./textures/${objName}.mtl)"></a-entity>`
 }
 
@@ -12,11 +12,11 @@ function afterEntity() {
     return "</a-nft>"
 }
 
-function addCamera(){
+function addCamera() {
     return "<a-entity camera></a-entity>";
 }
 
-function addModelFunction() {
+async function addModelFunction() {
     console.log("Attempting to load")
     let arjsScene = document.getElementById("arjsScene")
     console.log(arjsScene.innerHTML)
@@ -29,24 +29,26 @@ function addModelFunction() {
     }
     modelsPresent = true;
 
-    fetch('./filenames')
-        .then((response) => response.json())
-        .then((data) => {
-            for(let i = 0; i < data.length; i++) {
-                let obj = data[i];
-        
-                let markerUrl = obj["markerName"];
-                let modelUrl = obj["modelFile"];
+    const request = await fetch("./filnames", requestOptions);
+    if (!request.ok) {
+        console.log("error")
+    }
+    const data = await request.json();
 
-                markerUrl = markerUrl.split(".")[0]
-                modelUrl = modelUrl.split(".")[0]
-        
-                arjsScene.innerHTML += beforeEntity(markerUrl) + objEntity(modelUrl) + afterEntity();
-                console.log("Added: " + markerUrl + " " + modelUrl)
-            }
-        });
+    for (let i = 0; i < data.length; i++) {
+        let obj = data[i];
 
-    // arjsScene.innerHtml = arjsScene.innerHTML + addCamera();
+        let markerUrl = obj["markerName"];
+        let modelUrl = obj["modelFile"];
+
+        markerUrl = markerUrl.split(".")[0]
+        modelUrl = modelUrl.split(".")[0]
+
+        arjsScene.innerHTML += beforeEntity(markerUrl) + objEntity(modelUrl) + afterEntity();
+        console.log("Added: " + markerUrl + " " + modelUrl)
+    }
+
+    arjsScene.innerHTML = arjsScene.innerHTML + addCamera();
     arjsScene.innerHTML += "<canvas class=\"a-canvas\" data-aframe-canvas=\"true\" width=\"600\" height=\"300\"></canvas><div class=\"a-loader-title\" style=\"display: none;\"></div>"
     console.log(arjsScene.innerHTML)
     // console.log("added camera")
